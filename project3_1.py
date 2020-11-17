d34fc38c9bcae776d6fdca5e3d88203df4c4f734
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-11-17 15:41:06 +0900

    Update project3_1.py

--- project3_1.py
+++ project3_1.py
@@ -56,15 +56,16 @@ def showCorner(img, corners):
     return None
 
 def FourErrSorting(xLStandardSortL, FourErrorCoordinL, divFactor, gray_img):
-    slopeL = []; lengthL = []; angleL = []; justifyL = []; setJustifyL =[]
-
-    #=========================================4 general points lists=============================================
+    slopeL = []
+    lengthL = []
+    angleL = []
+    justifyL = []
+    setJustifyL =[]
     backOuterCoorL = []
 
-    #=============================================================================================================================================================================
-    #===================================================backOuter points are decided from 55-78(enhanced)=========================================================================
-    #=============================================================================================================================================================================
-    #==============================================appending required informations such as length and slope==================
+    # BackOuter points are decided from 66-118
+
+    # Appending required informations such as length and slope
     for each in FourErrorCoordinL:
         for eachInd in range(len(FourErrorCoordinL)):
             if FourErrorCoordinL[eachInd] == each: 
@@ -72,11 +73,10 @@ def FourErrSorting(xLStandardSortL, FourErrorCoordinL, divFactor, gray_img):
             else:
                 slopeL.append(slope(FourErrorCoordinL[eachInd][0], each[0], FourErrorCoordinL[eachInd][1], each[1]))
                 lengthL.append(pythagorasTheorem(FourErrorCoordinL[eachInd][0], each[0], FourErrorCoordinL[eachInd][1], each[1]))
-    #=============================================slope to angle============================================================= 
+    # Slope to angle 
     for each in slopeL:
         angleL.append(slope2Angle(each))
     #=============================================sorting things using length and angle======================================
-    print(lengthL)
     for each in range(len(lengthL)):
         if lengthL[each] > gray_img.shape[1]/4 and -20 <= angleL[each] <= 20:
             if each%divFactor > each//divFactor:
@@ -84,7 +84,7 @@ def FourErrSorting(xLStandardSortL, FourErrorCoordinL, divFactor, gray_img):
             else:
                 justifyL.append(FourErrorCoordinL[each%divFactor])
         elif lengthL[each] > gray_img.shape[1]/4 and 340 <= angleL[each] <= 0:
-            if each%9 > each//divFactor:
+            if each%divFactor > each//divFactor:
                 justifyL.append(FourErrorCoordinL[each%divFactor+1])
             else:
                 justifyL.append(FourErrorCoordinL[each%divFactor])
@@ -106,7 +106,6 @@ def FourErrSorting(xLStandardSortL, FourErrorCoordinL, divFactor, gray_img):
                 pass
             else:
                 lengthL.append(pythagorasTheorem(eachT[0], each[0], eachT[1], each[1]))
-    print(setJustifyL, lengthL)
     divLen = (len(setJustifyL)-1)
     IndA = (lengthL.index(max(lengthL)))%divLen
     IndB = lengthL.index(max(lengthL))//divLen
@@ -118,19 +117,18 @@ def FourErrSorting(xLStandardSortL, FourErrorCoordinL, divFactor, gray_img):
 
 def main(file_name):
     backBottomL = []
-    tempL = []
     slopeL = []
     slope2L = []
     angleL = []
+    tempL = []
+
     img = cv2.imread(file_name) 
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
-    #start_time = time.time()
     corners = cornerDetection(file_name, 0.1)
     #corners2 = cornerDetection(file_name, 0.0001)
-    #=============================key point ====================================
-    tempL = xLyLCorner(corners)
-    xL = tempL[0]
-    yL = tempL[1]
+
+    xL = xLyLCorner(corners)[0]
+    yL = xLyLCorner(corners)[1]
 
     #Make it unproceeding if you only want the data.
     showCorner(cv2.imread(file_name), corners)
@@ -139,26 +137,26 @@ def main(file_name):
     # Making lists for required information/sorted list using an axis as the base
     yLStandardSortL = doubleSort(yL, xL)#index 0 is yL
     # Raw list is required for sorting again!, so reset the list
-    xL = tempL[0]
-    yL = tempL[1]
+    xL = xLyLCorner(corners)[0]
+    yL = xLyLCorner(corners)[1]
 
     #===========================doubleSort the list again with x as its standardL================================
     xLStandardSortL = doubleSort(xL, yL)#index 0 is xL
     # All normal variables are ready now
-    divFactor = 3; FourErrorCoordinL = [[yLStandardSortL[1][each], yLStandardSortL[0][each]] for each in range(divFactor+1)]
-    backOuterCoorL = FourErrSorting(xLStandardSortL, FourErrorCoordinL, divFactor, gray_img)
+    DivFactor = 3; fourErrorCoordinL = [[yLStandardSortL[1][each], yLStandardSortL[0][each]] for each in range(DivFactor+1)]
+    backOuterCoorL = FourErrSorting(xLStandardSortL, fourErrorCoordinL, DivFactor, gray_img)
     
     # BackBottom Coordinates
 
     FrontOuterL = [[xLStandardSortL[0][0], xLStandardSortL[1][0]], [xLStandardSortL[0][-1], xLStandardSortL[1][-1]]]
-    
+
     # Create the equation for the indication line
-    slopeOfIndicationLine = (FrontOuterL[0][1]-FrontOuterL[1][1])/(FrontOuterL[0][0]-FrontOuterL[1][0])
-    c = FrontOuterL[0][1]-slopeOfIndicationLine*FrontOuterL[0][0]# Function can be called by saying: y = slopeOfIndicationLine * x + c
+    SlopeOfIndicationLine = (FrontOuterL[0][1]-FrontOuterL[1][1])/(FrontOuterL[0][0]-FrontOuterL[1][0])
+    C = FrontOuterL[0][1]-SlopeOfIndicationLine*FrontOuterL[0][0]# Function can be called by saying: y = slopeOfIndicationLine * x + c
 
     # Use the equation of the indication line for determining the backBottom point
     for each in range(len(yLStandardSortL[0])):
-        if yLStandardSortL[0][each] < slopeOfIndicationLine * yLStandardSortL[1][each] + c and min(backOuterCoorL[0][0], backOuterCoorL[1][0]) < yLStandardSortL[1][each] < max(backOuterCoorL[0][0], backOuterCoorL[1][0]):
+        if yLStandardSortL[0][each] < SlopeOfIndicationLine * yLStandardSortL[1][each] + C and min(backOuterCoorL[0][0], backOuterCoorL[1][0]) < yLStandardSortL[1][each] < max(backOuterCoorL[0][0], backOuterCoorL[1][0]):
             if [yLStandardSortL[1][each], yLStandardSortL[0][each]] not in backOuterCoorL:
                 tempL.append([yLStandardSortL[1][each], yLStandardSortL[0][each]])
 
@@ -194,23 +192,22 @@ def main(file_name):
             backleftTwoCoorInd = [backOuterCoorL[0], backBottomL[0]]; backrighttTwoCoorInd = [backOuterCoorL[1], backBottomL[1]]
         elif backBottomL[0][0] > backBottomL[1][0]:
             backleftTwoCoorInd = [backOuterCoorL[0], backBottomL[1]]; backrighttTwoCoorInd = [backOuterCoorL[1], backBottomL[0]]
+        
     elif backOuterCoorL[0][0] > backOuterCoorL[1][0]:
         if backBottomL[0][0] < backBottomL[1][0]:
             backleftTwoCoorInd = [backOuterCoorL[1], backBottomL[0]]; backrighttTwoCoorInd = [backOuterCoorL[0], backBottomL[1]]
         elif backBottomL[0][0] > backBottomL[1][0]:
             backleftTwoCoorInd = [backOuterCoorL[1], backBottomL[1]]; backrighttTwoCoorInd = [backOuterCoorL[0], backBottomL[0]]
 
-    #=============================================================================================================================================================================
-    #===========================================================Now, front len part of the whole code!============================================================================
-    #=============================================================================================================================================================================
-
     if backOuterCoorL[0][0] < backOuterCoorL[1][0]:
         backleftOuterCoor = backOuterCoorL[0]
         backrightOuterCoor = backOuterCoorL[1]
+    
     elif backOuterCoorL[0][0] > backOuterCoorL[1][0]:
         backleftOuterCoor = backOuterCoorL[1]
         backrightOuterCoor = backOuterCoorL[0]
-    print(backleftOuterCoor, backrightOuterCoor)
+    
+    # Front len part of the whole code
     '''
     #============================declare the lists that will be used to determine fl and fr==================================
     pointsForFl = []; pointsForFr = []
